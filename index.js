c5cd2415a6f56f8bebbe0797fded817816c25c78
AkhileshPS <akhileshpushpamangalam@gmail.com>
2025-04-02 19:36:46 +0530

    chng

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const itemRoutes = require('./itemRoutes');
 
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json()); 
