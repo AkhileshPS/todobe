2083fc74bd33a70af35266b5f189e334cbf20a43
AkhileshPS <akhileshpushpamangalam@gmail.com>
2025-04-03 09:33:50 +0530

    chaange

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://todofe2.vercel.app', // Replace with your actual frontend URL
-  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  credentials: true
+  origin: ['https://todofe2.vercel.app', 'http://localhost:3000'], // Allow both production and local frontend
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
 
 app.use(express.json()); 
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://akhilesh:akhilesh@todo.7wimxir.mongodb.net/?retryWrites=true&w=majority&appName=ToDo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("Connected to MongoDB Atlas"))
+mongoose.connect("mongodb+srv://akhilesh:akhilesh@todo.7wimxir.mongodb.net/?retryWrites=true&w=majority&appName=ToDo")
+  .then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.error("Database connection error:", err));
 
 // Routes
